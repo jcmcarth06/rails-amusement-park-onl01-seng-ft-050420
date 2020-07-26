77ca5a4c4559e9c3466bc1545226f9172b162327
scottvonscott <skeplin17@gmail.com>
2020-07-26 12:41:32 -0700

    setup 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :rides
     has_many :attractions, through: :rides
+    validates :name, :password, :nausea, :happiness, :tickets, :height, :admin, prescence: true
 end
--- db/migrate/20200725193802_create_rides.rb
+++ db/migrate/20200725193802_create_rides.rb
@@ -1,7 +1,6 @@
 class CreateRides < ActiveRecord::Migration[5.2]
   def change
     create_table :rides do |t|
-      t.string :name
       t.references :user, foreign_key: true
       t.references :attraction, foreign_key: true
 
