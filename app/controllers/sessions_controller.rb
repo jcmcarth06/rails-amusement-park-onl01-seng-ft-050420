0d6f80f9afb96f90f1b8f7f23794da1142d37996
scottvonscott <skeplin17@gmail.com>
2020-07-26 12:24:50 -0700

    More setup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+
+        # user = User.find_by(name: params[:user][:name])
+        # user = user.try(:authenticate, params[:user][:password])
+        # return redirect_to 'sessions/new' unless user
+        # session[:user_id] = user.id
+        # @user = user
+        # redirect_to '/'
+        
+        # if !params[:name] || params[:name].empty?
+        #     redirect_to '/login'
+        # else
+        #     session[:name] = params[:name]
+        #     redirect_to '/'
+        # end
+    end
+
+    def destroy
+        session.delete :name
+        redirect_to '/login'
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.create(user_params.merge(admin: false))
+        return redirect_to '/users/new' unless @user.save
+        session[:user_id] = @user.user_id
+        redirect_to '/'
     end
     
+    def private
+        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+    end
+
+
+
+       
 end
